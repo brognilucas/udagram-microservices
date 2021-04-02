4114dfe386c61645e5274b2522e7d3d272c9566e
Lucas Brogni <lucasbrogni16@gmail.com>
2021-04-02 15:40:23 +0100

    first commit project

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=udagramdb.cwxdhupi9h3q.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-brogni
+export AWS_REGION=eu-west-2
+export AWS_PROFILE=udagram
+export JWT_SECRET=s3cret
+export URL=http://localhost:8100
